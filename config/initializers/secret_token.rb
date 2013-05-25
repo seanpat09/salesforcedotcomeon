92683c581c4de0ae00c2deb4024c796906ffc01c
Sean Cuevo <seanpat09@gmail.com>
2013-05-24 20:35:38 -0600

    intial rails 4 boilerplate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Salesforcedotcomeon::Application.config.secret_key_base = '9c469dae8d7d37aab384c1469b811ccb648373ecab92056d23e47d9ace46aeb4770b50cdcefcc0afe6d1f83cb7fc4631a56a79d2bf35dfb9c155e6a8d5f9590c'
