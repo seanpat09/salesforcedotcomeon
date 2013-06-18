0077c116a339f9baa7e711380724596a92ed5064
Sean Cuevo <seanpat09@gmail.com>
2013-06-17 19:43:44 -0600

    Added length and presence validations for grievances

--- app/models/grievance.rb
+++ app/models/grievance.rb
@@ -1,2 +1,4 @@
 class Grievance < ActiveRecord::Base
+  validates_presence_of :content
+  validates :content, :length => { :maximum => 300 }
 end
--- test/models/grievance_test.rb
+++ test/models/grievance_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class GrievanceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save a post without content" do
+    grievance = Grievance.new
+    assert !grievance.save, "Saved without content"
+  end
+
+  test "should not save a post with more than 300 characters" do
+    grievance = Grievance.new
+    grievance.content = "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
+                         1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
+                         1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890"
+    assert !grievance.save, "Saved with more than 300 characters"
+  end
 end
